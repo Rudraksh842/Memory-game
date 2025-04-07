8663f1d62fa2485c532d2f244d0e3a29cec4498f
Rudraksh <76100454+Rudraksh842@users.noreply.github.com>
2025-04-07 21:59:16 +0530

    version 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import react from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import MemoryGame from "./componenets/memory-game";
+
+function App() {
+  return <MemoryGame />;
+}
+
+export default App;
